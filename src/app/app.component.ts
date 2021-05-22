e62947ed8ccdb19d08a9315db151b4d92c02a171
Amr Hesham <amrhesham1777@gmail.com>
2021-05-21 22:45:49 -0700

    Wasela

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss', './app.component.css']
+ 
+})
+export class AppComponent  implements OnInit{
+  title = 'wasila-angular';
+
+  constructor(public translate: TranslateService, private router : Router, private spinnerService: NgxSpinnerService,
+    private titleService: Title){
+
+  }
+
+  auth():boolean{
+    let value =  localStorage.getItem('UserToken')? true :false;
+   return value;
+ }
+
+ logout(){
+   localStorage.removeItem('UserToken');
+   console.log('logout')
+    this.router.navigate(['/login'])
+ }
+
+ ngOnInit() {
+  this.titleService.setTitle('وسيلة أون لاين');
+  this.spinner();
+  }
+  eng(){
+     localStorage.setItem('lang', 'en')
+     let lang : string = localStorage.getItem('lang') as string
+     this.translate.use(lang)
+  }
+  ar(){
+    localStorage.setItem('lang', 'ar')
+    let lang : string = localStorage.getItem('lang') as string
+    this.translate.use(lang)
+  }
+  spinner(): void {
+    this.spinnerService.show();
+    setTimeout(() => {
+      this.spinnerService.hide();
+    }, 4000);
+  }
+
+}
